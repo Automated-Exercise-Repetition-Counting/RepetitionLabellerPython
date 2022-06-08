388e6355d81430cb127d47d7b9743ae5c03c8013
Acediatic <hello@adamsinclair.kiwi>
2022-06-08 18:41:11 +1200

    feat: reads and displays image from np array

--- moment_classifier.py
+++ moment_classifier.py
@@ -69,63 +69,56 @@ def delete_frame(absolute_file_path):
 
 
 def get_current_iteration_from_file():
-    with open(OUTPUT_CSV_PATH, "r") as output_file:
-        lines = output_file.readlines()
-        if len(lines) == 0:
-            return 0
-        else:
-            return int(lines[-1].split(",")[0])
+    if os.path.exists(OUTPUT_CSV_PATH):
+        with open(OUTPUT_CSV_PATH, "r") as output_file:
+            lines = output_file.readlines()
+            if len(lines) > 0:
+                iteration, class_name = lines[-1].split(",")
+                return int(iteration), int(class_name)
+
+    return 0, INITIAL_MOMENT
 
 
 def classify_images(im_arr):
     max_iteration = im_arr.shape[0]
-    current_iteration = get_current_iteration_from_file()
-    current_moment = INITIAL_MOMENT
+    current_iteration, current_moment = get_current_iteration_from_file()
 
     while current_iteration <= max_iteration:
-        frame_name = get_frame_name_from_iteration(current_iteration)
-        frame_absolute_path = get_frame_absolute_path(frame_name)
-        frame = cv2.imread(frame_absolute_path)
-
-        if frame is None:
-            current_iteration += 1
-            continue
-
-        window_name = "Classify " + frame_name
+        window_name = f"Classify iteration {current_iteration}"
 
         cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty(window_name, cv2.WND_PROP_TOPMOST, 1)
         cv2.moveWindow(window_name, 0, 0)
-        cv2.imshow(window_name, frame)
+        cv2.imshow(window_name, im_arr[current_iteration])
 
         key = cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        match key:
-            case 113 | -1:
-                # q
-                exit()
-            case 110:
-                # n
-                update_frame_class(frame_name, current_moment)
-            case 98:
-                # b
-                remove_last_frame_update()
-                current_iteration -= 1
-                continue
-            case 32:
-                # space
-                current_moment = get_next_moment(current_moment)
-                print(f"\rCurrent Moment: {current_moment}", end="")
-                update_frame_class(frame_name, current_moment)
-            case 100:
-                # d
-                # delete the current frame
-                delete_frame(frame_absolute_path)
-            case _:
-                # any other key, repeat current iteration
-                print(f"Invalid key pressed: {key}")
-                continue
+        # match key:
+        #     case 113 | -1:
+        #         # q
+        #         exit()
+        #     case 110:
+        #         # n
+        #         update_frame_class(frame_name, current_moment)
+        #     case 98:
+        #         # b
+        #         remove_last_frame_update()
+        #         current_iteration -= 1
+        #         continue
+        #     case 32:
+        #         # space
+        #         current_moment = get_next_moment(current_moment)
+        #         print(f"\rCurrent Moment: {current_moment}", end="")
+        #         update_frame_class(frame_name, current_moment)
+        #     case 100:
+        #         # d
+        #         # delete the current frame
+        #         delete_frame(frame_absolute_path)
+        #     case _:
+        #         # any other key, repeat current iteration
+        #         print(f"Invalid key pressed: {key}")
+        #         continue
 
         current_iteration += 1
 
@@ -140,4 +133,4 @@ if __name__ == "__main__":
     np_im_arr = np_array_from_images()
     print("Done loading images.")
 
-    # classify_images(np_im_arr)
+    classify_images(np_im_arr)
